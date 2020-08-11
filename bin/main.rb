0e365d4aa7c9fe97218423297ca0d2ef017e6fa7
mohammadumar28 <mohammadumar28@gmail.com>
2020-08-11 16:05:31 +0530

    Weather feature added to the bot, get weather by city name

--- bin/main.rb
+++ bin/main.rb
@@ -16,8 +16,16 @@ def getWeather(city)
   json = Net::HTTP.get(uri)
   api_response = JSON.parse(json)
 
-  api_response['main']['temp']
+  return false if api_response['name'] == nil
+
+  info = {
+    name: api_response['name'],
+    temp: api_response['main']['temp']
+  }
+
+  info
 end
+p getWeather('iouuy')
 
 
 Telegram::Bot::Client.run(token) do |bot|
@@ -29,8 +37,13 @@ Telegram::Bot::Client.run(token) do |bot|
       bot.api.send_message(chat_id: message.chat.id, text: "Time is #{Time.now}, #{message.from.first_name}")
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Goodbye, #{message.from.first_name}")
-    when 'weather/chennai'
-      bot.api.send_message(chat_id: message.chat.id, text: "Weather in New York is, #{getWeather('chennai')} C")
+    when /^weather/
+      str = message.text.split('/')
+      if getWeather(str[1])
+        bot.api.send_message(chat_id: message.chat.id, text: "Weather in #{getWeather(str[1])[:name]} is, #{getWeather(str[1])[:temp]}")
+      else
+        bot.api.send_message(chat_id: message.chat.id, text: "City not found, please enter a valid city name.")
+      end
     else
       bot.api.send_message(chat_id: message.chat.id, text: 'I couldn\'t understand that command, please write a valid command.')
     end
