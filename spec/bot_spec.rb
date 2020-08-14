59522df0a2e219ffbac7d84f776ed89bfd6893f2
mohammadumar28 <mohammadumar28@gmail.com>
2020-08-14 09:38:02 +0530

    Changed context to describe

--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -9,7 +9,7 @@ describe 'TelegramBot' do
   let(:api_key) { ENV['OPENWEATHER_API'] }
   let(:invalid_key) { '1a2s3v4' }
 
-  context '#get_weather' do
+  describe '#get_weather' do
     it 'when an invalid API key' do
       weather_now = BotData.get_weather(invalid_key, 'London')
       expect(weather_now).to be false
@@ -21,7 +21,7 @@ describe 'TelegramBot' do
     end
   end
 
-  context '#covid_cases' do
+  describe '#covid_cases' do
     it 'when a valid country name is passed' do
       cov_cases = BotData.covid_cases('USA')
       expect(cov_cases).to be_a_kind_of(Hash)
@@ -33,56 +33,56 @@ describe 'TelegramBot' do
     end
   end
 
-  context '#weather_text' do
+  describe '#weather_text' do
     it 'when a valid argument (data) is passed' do
       weather = BotData.weather_text(weather_data)
       expect(weather).to be_a_kind_of(String)
     end
   end
 
-  context '#covid_text' do
+  describe '#covid_text' do
     it 'when a valid argument (data) is passed' do
       covid = BotData.covid_text(covid_data)
       expect(covid).to be_a_kind_of(String)
     end
   end
 
-  context '#welcome' do
+  describe '#welcome' do
     it 'returns string' do
       welcome = BotData.welcome
       expect(welcome).to be_a_kind_of(String)
     end
   end
 
-  context '#help' do
+  describe '#help' do
     it 'returns string' do
       help = BotData.help
       expect(help).to be_a_kind_of(String)
     end
   end
 
-  context '#unknown_command' do
+  describe '#unknown_command' do
     it 'returns string' do
       unknown_command = BotData.unknown_command
       expect(unknown_command).to be_a_kind_of(String)
     end
   end
 
-  context '#unknown_country' do
+  describe '#unknown_country' do
     it 'returns string' do
       unknown_country = BotData.unknown_country
       expect(unknown_country).to be_a_kind_of(String)
     end
   end
 
-  context '#unknown_city' do
+  describe '#unknown_city' do
     it 'returns string' do
       unknown_city = BotData.unknown_city
       expect(unknown_city).to be_a_kind_of(String)
     end
   end
 
-  context '#date' do
+  describe '#date' do
     it 'returns string' do
       date = BotData.date
       expect(date).to be_a_kind_of(String)
